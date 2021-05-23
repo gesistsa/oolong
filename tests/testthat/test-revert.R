9c67004b746e109a84053ba98af07849c0a47e38
chainsawriot <chainsawtiney@gmail.com>
2021-05-23 14:15:36 +0200

    Add revert tests

---
+++ tests/testthat/test-revert.R
@@ -0,0 +1,31 @@
+
+
+test_that("case in vignette", {
+    wsi_test <- readRDS(system.file("extdata", "wsi_test.RDS", package = "oolong"))
+    expect_error(revert_oolong(wsi_test, system.file("extdata", "hadley.RDS", package = "oolong")), NA)
+})
+
+massive_oolong <- readRDS("../testdata/massive_oolong.RDS")
+all_downloads_rds <- list.files("../testdata/downloaded/", full.names = TRUE)
+
+test_that("big_revert", {
+    expect_error(purrr::map(all_downloads_rds, ~revert_oolong(massive_oolong, .)), NA)
+    all_res <- purrr::map(all_downloads_rds, ~revert_oolong(massive_oolong, .))
+    expect_error(summarise_oolong(all_res[[1]], all_res[[2]], all_res[[3]], all_res[[4]], all_res[[5]]), NA)
+})
+
+
+test_that("verification unmatched test_content hash", {
+    hadley <- system.file("extdata", "hadley.RDS", package = "oolong")
+    ## no match
+    expect_error(revert_oolong(massive_oolong, hadley))
+})
+
+test_that("cheating", {
+    cheater <- readRDS(all_downloads_rds[1])
+    cheater$test_content$answer <- "wrong"
+    saveRDS(cheater, "cheater.RDS")
+    expect_error(revert_oolong(massive_oolong, cheater))
+    unlink("cheater.RDS")
+})
+
